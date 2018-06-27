2543c0882ffa0f4e2b62a98f536773a06509020d
Algonzales <gonzo2356@gmail.com>
2018-06-27 17:12:14 -0500

    Edited comments

--- FunBoard.cpp
+++ FunBoard.cpp
@@ -2,9 +2,6 @@
     Name: Alexander Gonzales
     Build: 1.0
     Date: Dec 13, 2017
-    Class: CSC 310
-    Instructor: Lori Nicholson
-    Project: Final Project
     Description: This project deals with a 4x4 game board. Players can put no two letters or numbers in
     the same vertical or horizontal on a grid
 */
